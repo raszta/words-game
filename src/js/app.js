4490ec7409bd29f1f19bb403a3183cc593d92d41
Imię i nazwiskoPiotr <raszta9999@wp.pl>
2018-09-19 21:32:48 +0200

    Random passwords

--- src/js/app.js
+++ src/js/app.js
@@ -1,33 +1,32 @@
 $(()=>{
-    const $gameAttempts = $('.game-attempts');
-    const $gameSentence = $('.game-sentence');
-    const $elemLetters = $('.game-letters');
+    const $gameAttempts = $( '.game-attempts' );
+    const $gameSentence = $( '.game-sentence' );
+    const $elemLetters = $( '.game-letters' );
+    const $startBtn = $( '.game-start__btn' );
+
 
     class Game {
         constructor(){ 
-            this.attempts = 5;
+            this.attempts = 0;
             const passwords = [ 'Terminator',
             'Harry Potter i kamień filozoficzny',
-            'Drużyna A'];
+            'Drużyna A' ];
             this.currentPass = null;
 
             this.generateLetters =function () {
-                const alphabet = ['a', 'ą', 'b', 'c', 'ć', 'd', 'e', 'ę', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'ł', 'm', 'n', 'ń', 'o', 'ó', 'p', 'q', 'r', 's', 'ś', 't', 'u', 'v',  'x', 'y', 'z', 'ż', 'ź'];
+                const alphabet = [ 'a', 'ą', 'b', 'c', 'ć', 'd', 'e', 'ę', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'ł', 'm', 'n', 'ń', 'o', 'ó', 'p', 'q', 'r', 's', 'ś', 't', 'u', 'v',  'x', 'y', 'z', 'ż', 'ź' ];
 
                 alphabet.forEach(( letter )=>{
                     const $btn = $(`<button data-id=${letter}>${letter}</button>`);
                     $btn.addClass('game-letter');
-                    $elemLetters.append( $btn );
-                                       
+                    $elemLetters.append( $btn );                                       
                 });
             },
 
             this.bindClick = () =>{
-               $elemLetters.on( 'click', function(e){
+               $elemLetters.on( 'click', function( e ){
                    const letter = $(e.target).data('id');
-                    console.log(letter);
-                   $(e.target).attr('disabled', true );
-                   
+                   $( e.target ).attr( 'disabled', true );                  
                     
                 });
             }
@@ -35,16 +34,41 @@ $(()=>{
             this.initBoard =  () =>{
                 this.generateLetters();
                 this.bindClick();
+                this.disableLetters();               
+            }
+
+            this.enableLetters = () =>{
+                const letters = document.querySelectorAll( '.game-letter');
+                letters.forEach(el => $(el).attr( 'disabled', false));
+            }
+
+            this.disableLetters = () =>{
+                const letters = document.querySelectorAll( '.game-letter');
+                letters.forEach(el => $(el).attr( 'disabled', true));
+            }
+
+            this.showAttempts = () =>{
+                $gameAttempts.text(this.attempts);
+            }
+
+            this.randomSentence = ()=>{
+                this.currentPass = passwords[Math.floor(Math.random()*passwords.length)];
+                
+            }
+
+            this.startGame = () =>{
+                this.attempts = 4;
+                this.enableLetters();
+                this.showAttempts();
+                this.randomSentence();
             }
         }
     }
-   
-    const $startBtn = $( '.game-start__btn' );
-    
+       
     $startBtn.on( 'click', () =>{
         const game = new Game();
         game.initBoard();
-        $startBtn.attr('disabled', true);
+        $startBtn.attr( 'disabled', true);        
     });
 
 });
