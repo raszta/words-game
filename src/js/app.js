ae25d1e48c737e000bf168aa8baf391749febd0a
Imię i nazwiskoPiotr <raszta9999@wp.pl>
2018-09-19 22:32:28 +0200

    New styling, randomSentence improved

--- src/js/app.js
+++ src/js/app.js
@@ -3,15 +3,16 @@ $(()=>{
     const $gameSentence = $( '.game-sentence' );
     const $elemLetters = $( '.game-letters' );
     const $startBtn = $( '.game-start__btn' );
-
+    const $prepare = $('.prepare');
 
     class Game {
         constructor(){ 
             this.attempts = 0;
             const passwords = [ 'Terminator',
-            'Harry Potter i kamień filozoficzny',
+            'Forrest gump', 'Pitch black',
             'Drużyna A' ];
             this.currentPass = null;
+            this.currentPassLetters = null;
 
             this.generateLetters =function () {
                 const alphabet = [ 'a', 'ą', 'b', 'c', 'ć', 'd', 'e', 'ę', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'ł', 'm', 'n', 'ń', 'o', 'ó', 'p', 'q', 'r', 's', 'ś', 't', 'u', 'v',  'x', 'y', 'z', 'ż', 'ź' ];
@@ -48,26 +49,46 @@ $(()=>{
             }
 
             this.showAttempts = () =>{
-                $gameAttempts.text(this.attempts);
+                $gameAttempts.text(`Your attempts: ${this.attempts}`);
             }
 
             this.randomSentence = ()=>{
                 this.currentPass = passwords[Math.floor(Math.random()*passwords.length)];
+                this.currentPassLetters = this.currentPass.replace(/ /g, '');
+                console.log(this.currentPass);
                 
+                $gameSentence.innerHtml = '';
+                const letters = this.currentPass.split('');
+
+                letters.forEach(el =>{
+                    const $div = $('<div>');
+                    $div.addClass('game-letter-box');
+                    if(el === ' '){
+                        $div.addClass('game-letter-box--without');
+                    }
+                    $gameSentence.append($div);
+                });
             }
 
             this.startGame = () =>{
+                this.initBoard();
                 this.attempts = 4;
-                this.enableLetters();
+                
+                $prepare.show();      
+                setTimeout(()=>{
+                    $prepare.hide();          
+                    this.enableLetters();
+                },2000);
                 this.showAttempts();
                 this.randomSentence();
             }
         }
     }
-       
+    $prepare.hide();  
+
     $startBtn.on( 'click', () =>{
         const game = new Game();
-        game.initBoard();
+        game.startGame();
         $startBtn.attr( 'disabled', true);        
     });
 
