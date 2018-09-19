ed75d499a19ddd1c93bd9edca97784f467d108c6
Imię i nazwiskoPiotr <raszta9999@wp.pl>
2018-09-19 18:49:58 +0200

    Adding click events to letters

--- src/js/app.js
+++ src/js/app.js
@@ -1,26 +1,53 @@
 $(()=>{
+    const $gameAttempts = $('.game-attempts');
+    const $gameSentence = $('.game-sentence');
+
 
     class Game {
-        constructor(){      
-        this.elemLetters =  $('.game-letters'),
+        constructor(){  
+            this.elemLetters = $('.game-letters');
 
-        this.generateLetters= function(){
-            const alphabet = ['a', 'ą', 'b', 'c', 'ć', 'd', 'e', 'ę', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'ł', 'm', 'n', 'o', 'ó', 'p', 'q', 'r', 's', 'ś', 't', 'u', 'v',  'x', 'y', 'z', 'ż', 'ź'];
+            this.attempts = 5;
+            const passwords = [ 'Terminator',
+            'Harry Potter i kamień filozoficzny',
+            'Drużyna A'];
+            this.currentPass = null;
 
-            alphabet.forEach((letter)=>{
-                const btn = $('<button>');
-                btn.text(letter);
-                btn.data('id','letter');
-                this.elemLetters.append(btn);
-            });
-        },
+            this.generateLetters =function () {
+                const alphabet = ['a', 'ą', 'b', 'c', 'ć', 'd', 'e', 'ę', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'ł', 'm', 'n', 'ń', 'o', 'ó', 'p', 'q', 'r', 's', 'ś', 't', 'u', 'v',  'x', 'y', 'z', 'ż', 'ź'];
 
-        this.initBoard=function () {
-            this.generateLetters();
+                alphabet.forEach(( letter )=>{
+                    const $btn = $( '<button>' );
+                    $btn.text( letter );
+                    $btn.data( 'id', letter );
+                    
+                    this.elemLetters.append( $btn );
+                                       
+                });
+            },
+
+            this.bindClick = () =>{
+                this.elemLetters.on( 'click', function(e){
+                    const letter = e.target;
+                    console.log(letter);
+                   
+                    
+                });
+            }
+
+            this.initBoard =  () =>{
+                this.generateLetters();
+                this.bindClick();
+            }
         }
     }
-    }
-    const game = new Game();
-    game.initBoard();
+   
+    const $startBtn = $( '.game-start__btn' );
     
+    $startBtn.on( 'click', () =>{
+        const game = new Game();
+        game.initBoard();
+        
+    });
+
 });
